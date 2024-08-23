472a7e31e01ddc277866697f533bcd1dbcfdf809
rahulbera9386 <rahulberastd2002@gmail.com>
2024-08-23 11:59:23 +0530

    created company model

---
+++ server/models/company.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    website: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+    },
+    logo: {
+      type: String,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Company=mongoose.model("Company",companySchema);
+export default Company;
