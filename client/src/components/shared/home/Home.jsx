87e55b1aef203549325cacbfdd5b4525b501013b
rahulbera9386 <rahulberastd2002@gmail.com>
2024-08-29 15:09:53 +0530

    Feat:footer Section

--- client/src/components/shared/home/Home.jsx
+++ client/src/components/shared/home/Home.jsx
@@ -3,15 +3,19 @@ import Navbar from './../navbar/Navbar';
 import HeroSection from "../hero/HeroSection";
 import CategoryCarousel from "../categorycarousel/CategoryCarousel";
 import LatestJobs from "../latestjobs/LatestJobs";
+import Footer from "../footer/Footer";
 
 const Home = () => {
   return (
+    <>
     <div className="max-w-7xl mx-auto">
       <Navbar />
       <HeroSection/>
       <CategoryCarousel/>
       <LatestJobs/>
     </div>
+    <Footer/>
+    </>
   );
 };
 
