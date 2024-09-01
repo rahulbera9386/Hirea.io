ac3f39789f386ac70d619c6d144b84efab30a2a5
rahulbera9386 <rahulberastd2002@gmail.com>
2024-09-01 16:37:27 +0530

    push

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
+app.get("/",()=>{console.log("Backend Started")})
 
 app.listen(PORT,()=>{
     connectDB();
